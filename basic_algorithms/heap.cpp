832f07f977618a8de8e865c6535008abfac0f43d
askaidaisuke <mandm6523@gmail.com>
2021-12-14 10:16:54 +0900

    add heap sort

--- basic_algorithms/heap.cpp
+++ basic_algorithms/heap.cpp
@@ -8,35 +8,44 @@ void swap(std::vector<int>* array, int i, int j){
 	(*array)[j] = temp;
 }
 
-void heapify(std::vector<int>* array, int top){
+void heapify(std::vector<int>* array, int top, int tail){
 	int left = top*2+1;
 	int right = top*2+2;
 	int largest = top;
 
-	if((*array)[largest] < (*array)[left]){
+	if(tail >= left && (*array)[largest] < (*array)[left]){
 		largest = left;
 	}
-	if((*array)[largest] < (*array)[right]){
+	if(tail >= right &&(*array)[largest] < (*array)[right]){
 		largest = right;
 	}
 	if(largest != top){
 		swap(array, top, largest);
-		heapify(array, largest);
+		heapify(array, largest, tail);
 	}
 	
 }
 
 void build_heap(std::vector<int>* array){
 	int start_index = array->size() / 2 - 1;
+	int tail = array->size() -1;
 	for(int i=start_index; i > -1; i--){
-		heapify(array, i);
+		heapify(array, i, tail);
 	}
 	
 }
 
+void heap_sort(std::vector<int>* array){
+	build_heap(array);
+	for(int i=array->size()-1; i<-1; i--){
+		swap(array, 0, i);
+		heapify(array, 0, i);
+	}
+}
+
 void print(std::vector<int>* array){
 	for(auto ele: *array){
-		std::cout << ele;
+		std::cout << ele << " ";
 	}
 	std::cout << std::endl;
 }
@@ -49,6 +58,6 @@ int main(int argc, char* argv[]){
 		std::cin >> array[i];
 	}
 
-	build_heap(&array);
+	heap_sort(&array);
 	print(&array);
 }
